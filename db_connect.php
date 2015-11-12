d704200ae6adc7809602a1b08932f41280848bde
derekthederek <derekthehyde@gmail.com>
2015-11-12 14:05:34 +0000

    initializing site

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = getenv('IP');
+    $username = getenv('C9_USER');
+    $password = "";
+    $database = "c9";
+    $dbport = 3306;
+
+    // Create connection
+    $mysqli = new mysqli($servername, $username, $password, $database, $dbport);
+
+    // Check connection
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    } 
+?>
