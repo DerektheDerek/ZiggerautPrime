d65434983d505d49e7c50a9d413e97d4af8fb714
derekthederek <derekthehyde@gmail.com>
2015-11-12 20:25:57 +0000

    created boilerplate for webapp

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Ziggeraut Prime</title>
+        <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="css/materialize.css" media="screen"/>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+        <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+     <div class="above-nav"></div>
+        <nav role="navigation">
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo nav-logo">ZIGGERAUT PRIME</a>
+              <a href="#" data-activates="nav-mobile" class="button-collapse the-menu"><i class="material-icons">menu</i></a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#" class="waves-effect waves-light nav-btn active">ABILITIES</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">WEAPONS</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">TRACKER</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">LOGIN</a></li>
+              </ul>
+              <ul class="side-nav" id="mobile-demo">
+                <li><a href="#" class="waves-effect waves-light nav-btn active">ABILITIES</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">WEAPONS</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">TRACKER</a></li>
+                <li><a href="#" class="waves-effect waves-light nav-btn">LOGIN</a></li>
+              </ul>
+            </div>
+        </nav>
+        <div class="container main">
+           
+        </div>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript" src="js/materialize.js"></script>
+        <script type="text/javascript" src="DataTables/datatables.js"></script>
+        <script type="text/javascript">
+            $(".button-collapse").sideNav();
+        </script>
+    </body>
+</html>
